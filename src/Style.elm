c6825b4c2fa3fe76c8fe9f9d66eeb0932ef54994
Paul Mathews <pfcmathews@gmail.com>
2017-07-07 00:05:32 +1200

    tidy up, still won't build css

--- src/Style.elm
+++ src/Style.elm
@@ -1,10 +1,7 @@
-module Style exposing (..)
+module Style exposing (css)
 
-import Html
-import Html.Attributes
-import Html.CssHelpers exposing (withNamespace)
 import Css exposing (..)
-import Css.Elements
+import Css.Elements exposing (..)
 import Css.Namespace exposing (namespace)
 
 import SharedStyles exposing (..)
@@ -15,37 +12,37 @@ import SharedStyles exposing (..)
 
 bannerStyle =
   [ backgroundColor (hex "#edeae3")
-  , padding4 (em 5) (em 2) (em 4.25) (em 2)
-  , marginBottom (em -2.5) ]
+  , padding4 (Css.em 5) (Css.em 2) (Css.em 4.25) (Css.em 2)
+  , marginBottom (Css.em -2.5) ]
 
 -- style basics that everyone should inherit
 defaultStyle =
   [ color (hex "#44140")
   , fontFamilies [ "Avenir", "Helvetica Neue" ]
-  , letterSpacing (em 0.1)]
+  , letterSpacing (Css.em 0.1)]
 
 floraHeaderStyle =
-  [ fontSize (em 1.5)
-  , marginBottom (em 0.2)
-  , paddingBottom (em 0.1)
+  [ fontSize (Css.em 1.5)
+  , marginBottom (Css.em 0.2)
+  , paddingBottom (Css.em 0.1)
   , color (hex "#444140")
   , textAlign center
   , fontWeight (int 700)
-  , letterSpacing (em 0.1)
-  , margin4 (em 0) (em 0) (em 1) (em 0)
+  , letterSpacing (Css.em 0.1)
+  , margin4 (Css.em 0) (Css.em 0) (Css.em 1) (Css.em 0)
   , borderBottom3 (px 2) solid (rgba 255 255 255 0.125)]
 
 projectHeaderStyle =
-  [ fontSize (em 2.25)
-  , paddingBottom (em 0.1)
+  [ fontSize (Css.em 2.25)
+  , paddingBottom (Css.em 0.1)
   , textAlign center
-  , margin4 (em 0) (em 0) (em 0.1) (em 0) ]
+  , margin4 (Css.em 0) (Css.em 0) (Css.em 0.1) (Css.em 0) ]
 
 innerStyle =
   [ margin auto ]
 
 descriptionStyle =
-  [ fontSize (em 0.8)
+  [ fontSize (Css.em 0.8)
   , fontWeight (int 200)
   , lineHeight (int 2)
   , textTransform lowercase ]
