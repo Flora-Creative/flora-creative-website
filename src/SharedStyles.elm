986d61579e2e959f4fccc8bfe7c104545d22a32b
Paul Mathews <pfcmathews@gmail.com>
2017-07-09 12:59:06 +1200

    styling for app views

--- src/SharedStyles.elm
+++ src/SharedStyles.elm
@@ -4,10 +4,15 @@ import Html.CssHelpers exposing (withNamespace)
 
 -- Style types and helpers that everyone needs to see
 -- This is the top level stuff, submodules etc. can have their own
+-- you will just have to make sure the generator is aware by editing
+-- Stylesheets.elm
 
 
 type CssClasses = Inner
                 | Description
+                | Content
+                | Wrapper
+                | Spacer
 
 
 type CssIds = Banner
--- src/Stylesheets.elm
+++ src/Stylesheets.elm
@@ -2,13 +2,20 @@ port module Stylesheets exposing (..)
 
 import Css.File exposing (CssFileStructure, CssCompilerProgram)
 import Style
+import Apps.Style
 
 port files : CssFileStructure  -> Cmd msg
+
+-- all the CSS to concatenate
+allCss =
+  [ Style.css
+  , Apps.Style.css
+  ]
 
 fileStructure : CssFileStructure
 fileStructure =
   Css.File.toFileStructure
-    [ ( "site/gen/css/index.css", Css.File.compile [ Style.css ] ) ]
+    [ ( "site/gen/css/index.css", Css.File.compile allCss ) ]
 
 
 main : CssCompilerProgram
