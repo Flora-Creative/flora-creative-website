daa68d57110b8b3af33f182403e577288c1e951e
Paul Mathews <pfcmathews@gmail.com>
2017-07-09 15:21:48 +1200

    pull the view and types together

--- src/State.elm
+++ src/State.elm
@@ -1,35 +1,47 @@
 module State exposing (initialState, update, subscriptions)
 
 import Dict
+import Debug exposing (log)
 
 import Types exposing (..)
+import Apps.State
+
 
 -- This is where we deal with the updates to the model etc.
-
-
 --UPDATE: update an app's details or scroll us to one
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    AppLoad result ->
-      -- decode app and assign into model
-      (model, Cmd.none)
+    AppsLoad (Ok result) ->
+      -- decode apps and assign into model
+      ( { model | apps = Apps.State.decodeAppsList result }, Cmd.none )
+
+    AppsLoad (Err error) ->
+      -- ignore errors for now
+      let
+        err = log "error " error
+      in
+        ( model,  Cmd.none )
 
     AppClick clickedApp ->
       -- just update the current focus
-      (model, Cmd.none)
+      ( model, Cmd.none )
 
 
 -- SUBSCRIPTIONS: probably none?
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
 
+
 -- INIT: kick off loading the apps
 
-initialState : String -> (Model, Cmd Msg)
+
+initialState : String -> ( Model, Cmd Msg )
 initialState databaseURL =
-  (Model Dict.empty Nothing , Cmd.none)
+  ( Model Dict.empty Nothing, Apps.State.getAll databaseURL )
--- src/Types.elm
+++ src/Types.elm
@@ -3,19 +3,20 @@ module Types exposing (..)
 import Dict
 import Http
 
-import Apps.Types exposing (App)
+import Apps.Types exposing (AppModel)
+import Generated.AppAPI exposing (Floraapps)
 
 -- The types that everyone needs to see
 
 -- MODEL: what should it be? I don't think a lot
 
 type alias Model =
-  { apps: Dict.Dict String App
+  { apps: Dict.Dict String AppModel
   , focus: Maybe String
   }
 
 
 -- MESSAGES: an app has loaded or someone clicked on the little nav guy
 
-type Msg = AppLoad (Result Http.Error String)
+type Msg = AppsLoad (Result Http.Error (List Floraapps))
          | AppClick String
