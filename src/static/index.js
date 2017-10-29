5fa30c7030a3fd365b74e554826579de6693654a
Paul Mathews <pfcmathews@gmail.com>
2017-10-29 19:34:08 +1300

    start moving to webpack for nicer builds of js + elm etc.

---
+++ src/static/index.js
@@ -0,0 +1,5 @@
+var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
+
+// inject bundled Elm app into div#main
+var Elm = require( '../elm/Main' );
+Elm.Main.embed( document.getElementById( 'main' ) );
